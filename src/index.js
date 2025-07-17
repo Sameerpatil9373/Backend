7a455ae95f5fe4aa8fd6e3bf1e66e2224bf97d77
Sameerpatil9373 <sameerpatil9373@gmail.com>
2025-07-17 18:00:15 +0530

    removed node_modules from tracking

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-require('dotenv').config({path:'./env'});
+//require('dotenv').config({path:'./env'});
 
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import connectDB from "./db";
-
-
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 connectDB()
 .then(() =>{
-     app.listen(process.env.PORT || 8000)
+     app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running ${process.env.PORT}`);
+     })
 })
 .catch((err) =>{
     console.log("mongodb failed",err);
