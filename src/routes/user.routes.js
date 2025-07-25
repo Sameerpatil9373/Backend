cf9548efef8596f2f6a675635f7c28bd55a97c3f
Sameerpatil9373 <sameerpatil9373@gmail.com>
2025-07-25 07:33:31 +0530

    #

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUseravatar, UpdateCoverImage, getChannelProfile, getwatchhistory } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,5 +20,13 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUseravatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),UpdateCoverImage)
+router.route("/c/:username").get(verifyJWT,getChannelProfile)
+router.route("/history").get(verifyJWT,getwatchhistory)
 
 export default router
